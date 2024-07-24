ca0b6fb014b22f8639210e03ce84cf272e8886f7
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-07-24 10:05:22 +0000

    commit

---
+++ .vscode/tree/basics.java
@@ -0,0 +1,20 @@
+// Trees
+// collection of nodes
+
+// types
+//  =>> binary {max 2 children}
+//  =>> ternary {at most 3 child}
+//  =>> n-array {multiple nodes}
+
+class basics{
+
+
+
+
+
+
+
+
+
+    
+}
