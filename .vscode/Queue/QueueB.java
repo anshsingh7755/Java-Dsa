2ede955653238db271c857fa0795b16aa891aa55
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-07-25 15:36:48 +0000

    commit

--- .vscode/Queue/QueueB.java
+++ .vscode/Queue/QueueB.java
@@ -19,7 +19,48 @@ public class QueueB{
         public static void add(int data){
             if(rear == size-1){
                 System.out.println("Queue is full: ");
+                return;
+            }
+
+            rear = rear + 1;
+            arr[rear] = data;
+        }
+
+        // remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i+1];
+            }
+            rear = rear - 1;
+            return front;
+        }
+
+        // peek
+        public static int peek(){
+            if(isEmplty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+            return arr[0];
+        }
+
+        public static void main(String[] args) {
+            Queue q = new Queue(5);
+            q.add(1);
+            q.add(2);
+            q.add(3);
+
+            while(!q.isEmpty()){
+                System.out.println(q.peek());
+                q.remove();
             }
         }
     }
+
 }
