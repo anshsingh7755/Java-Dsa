654959d4b16226a4c9939472353b9366e97d7d07
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-07-25 10:22:19 +0000

    A

---
+++ .vscode/Graphs/Basics.java
@@ -0,0 +1,15 @@
+// A Graph is a non-linear data structure that consists of vertices (nodes) and edges.
+
+//  properties
+// => Weighted
+// => Connected
+// => Directed
+// => Cyclic
+// => Loop
+
+// Adjacency matrix and graph 
+// The Adjacency Matrix is a 2D array (matrix) where each cell on index (i,j) 
+// stores information about the edge from vertex i to vertex j.
+
+// Adjacency List Graph Representation??
+
