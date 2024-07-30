8a47a631f864b7f4cf82d117d6b20c6eb5c80ac5
anshsingh7755 <121092055+anshsingh7755@users.noreply.github.com>
2024-07-30 06:45:53 +0000

    A

---
+++ .vscode/Heap/Basics.java
@@ -0,0 +1,19 @@
+// A Heap is a complete binary tree data structure that satisfies the heap property: 
+// for every node, the value of its children is greater than or equal to its own value
+
+
+// Max Heap: The root node contains the maximum value, and the values decrease as you move down the tree.
+// A[Parent(i)] >= A[i]
+
+
+// Min Heap: The root node contains the minimum value, and the values increase as you move down the tree.
+// A[Parent(i)] <= A[i]
+
+// Applications
+// Heaps are commonly used to implement priority queues, where elements are retrieved based on their priority (maximum or minimum value).
+// Heapsort is a sorting algorithm that uses a heap to sort an array in ascending or descending order.
+// Heaps are used in graph algorithms like Dijkstra’s algorithm and Prim’s algorithm for finding the shortest paths and minimum spanning trees.
+
+
+
+
